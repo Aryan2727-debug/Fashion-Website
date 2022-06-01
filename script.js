94649ae4685f88d246b592a59cb01fda6e865a50
Aryan2727-debug <78311368+Aryan2727-debug@users.noreply.github.com>
2022-06-01 22:41:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function abc(){
+    alert("You are successfully registered on our App!");
+  }
+  function xyz(){
+    alert("Your account has been successfully created!");
+  }
+  function def(){
+    alert("Membership successfully bought!");
+  }
